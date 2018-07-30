489d53f0c77e9aba81518cef850fcf6ca065812f
Ryan Jennings <ryan@coda.life>
2018-07-29 22:25:44 -0700

    Update for demo

--- scheduling/lottery.c
+++ scheduling/lottery.c
@@ -15,6 +15,7 @@
 // a lottery type
 typedef struct lottery Lottery;
 
+// a callback to distribute tickets
 typedef int (*OnDistribution)(Lottery *);
 
 // lottery data
@@ -27,6 +28,7 @@ struct lottery {
   Queue *queue;
 };
 
+// Memory allocation
 Lottery *new_lottery(OnDistribution distributer) {
   Lottery *l = malloc(sizeof(Lottery));
   if (l == NULL) {
@@ -44,19 +46,6 @@ void delete_lottery(Lottery *l) {
   }
   delete_queue_list(l->queue);
   free(l);
-}
-
-// simply iterates processes to find the total service time
-static int __process_service_time_iterator(Queue *queue, int index, Process *p, void *arg) {
-  if (queue == NULL || index == -1 || p == NULL || arg == NULL) {
-    return -1;
-  }
-
-  int *total = (int *) arg;
-
-  *total += process_current_service_time(p);
-
-  return QUEUE_ITERATE_NEXT;
 }
 
 static int __lottery_arrive(Process *p, void *arg) {
@@ -81,58 +70,6 @@ static int __lottery_ready(void *arg) {
   }
   Lottery *l = (Lottery*) arg;
   return !queue_is_empty(l->queue);
-}
-
-// a simple lottery that divides up tickets evenly based on queue size
-int __distribution_simple(Lottery *l) {
-  if (l == NULL) {
-    return -1;
-  }
-
-  const int qsize = queue_size(l->queue);
-
-  // the bucket size
-  int bucket = NUM_TICKETS / qsize;
-
-  for (int i = 0, p = 0; i < NUM_TICKETS; i++) {
-    l->ticket_distribution[i] = p;
-
-    if (i == bucket) {
-      p++;
-      bucket += bucket;
-    }
-  }
-
-  return 0;
-}
-
-// an algorithm to auto-assign tickets and a winner
-int  __distribution_service_time(Lottery *l) {
-  if (l == NULL) {
-    return -1;
-  }
-
-  int total_service_time = 0;
-
-  // get the total service time of the queue
-  if (queue_iterate(l->queue, __process_service_time_iterator, &total_service_time)) {
-    return -1;
-  }
-
-  for (int i = 0, j = -1, pos = 0; i < NUM_TICKETS; i++) {
-    l->ticket_distribution[i] = j;
-
-    if (i >= pos) {
-      Process *p = queue_peek_at(l->queue, ++j);
-
-      // get the next process ticket count
-      int tickets = (((float) process_current_service_time(p) / (float) total_service_time) * (float) NUM_TICKETS);
-
-      pos += tickets;
-    }
-  }
-
-  return 0;
 }
 
 static Process *__lottery_get(void *arg) {
@@ -168,12 +105,79 @@ static int __lottery_put(Process *p, void *arg) {
   return l->on_distribution(l);
 }
 
+// a simple lottery that divides up tickets evenly based on queue size
+int __distribution_simple(Lottery *l) {
+  if (l == NULL) {
+    return -1;
+  }
+
+  const int qsize = queue_size(l->queue);
+
+  // the bucket size
+  int bucket = NUM_TICKETS / qsize;
+
+  for (int i = 0, p = 0; i < NUM_TICKETS; i++) {
+    l->ticket_distribution[i] = p;
+
+    if (i == bucket) {
+      p++;
+      bucket += bucket;
+    }
+  }
+
+  return 0;
+}
+
+// simply iterates processes to find the total service time
+static int __process_service_time_iterator(Queue *queue, int index, Process *p, void *arg) {
+  if (queue == NULL || index == -1 || p == NULL || arg == NULL) {
+    return -1;
+  }
+
+  int *total = (int *) arg;
+
+  *total += process_current_service_time(p);
+
+  return QUEUE_ITERATE_NEXT;
+}
+
+
+// an algorithm to auto-assign tickets and a winner
+int  __distribution_service_time(Lottery *l) {
+  if (l == NULL) {
+    return -1;
+  }
+
+  int total_service_time = 0;
+
+  // get the total service time of the queue
+  if (queue_iterate(l->queue, __process_service_time_iterator, &total_service_time)) {
+    return -1;
+  }
+
+  for (int i = 0, j = -1, pos = 0; i < NUM_TICKETS; i++) {
+    l->ticket_distribution[i] = j;
+
+    if (i >= pos) {
+      Process *p = queue_peek_at(l->queue, ++j);
+
+      // get the next process ticket count
+      int tickets = (((float) process_current_service_time(p) / (float) total_service_time) * (float) NUM_TICKETS);
+
+      pos += tickets;
+    }
+  }
+
+  return 0;
+}
+
+
 int main() {
 
   srand(time(0));
 
   // a new lottery
-  Lottery *lottery = new_lottery(__distribution_simple);
+  Lottery *lottery = new_lottery(__distribution_service_time);
 
   // create the algorithm
   Algorithm *algo = new_algorithm(__lottery_arrive, __lottery_ready, __lottery_get, __lottery_put, lottery);
